95b21b5fe3c808e24728aebae67df7fb5b451249
AbhishekYadav-09 <abhishekyadavcode@gmail.com>
2025-12-27 20:34:51 +0530

    fix: error

--- src/component/beyondTheCode.component.jsx
+++ src/component/beyondTheCode.component.jsx
@@ -47,7 +47,7 @@ export default function BeyondTheCode() {
           Journey Timeline
         </button>
 
-        {/* <button
+        <button
           onClick={() => setActiveTab("articles")}
           className={`px-6 py-3 rounded-xl font-semibold transition-all duration-300 ${
             activeTab === "articles"
@@ -56,7 +56,7 @@ export default function BeyondTheCode() {
           }`}
         >
           Articles
-        </button> */}
+        </button>
         
       </div>
 
